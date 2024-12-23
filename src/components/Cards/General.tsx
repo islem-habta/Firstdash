cb710ea2566d22cd3c87e4375480eeb8225315d1
islem~ <islemhabta@gmail.com>
2024-12-23 19:48:39 +0100

    Update General.tsx component with recent changes

--- src/components/Cards/General.tsx
+++ src/components/Cards/General.tsx
@@ -7,22 +7,23 @@ import {
     CardTitle,
     CardDescription,
 } from "@/components/ui/card";
-import { ResponsiveBump, BumpDatum } from "@nivo/bump";
+import { ResponsiveBump, BumpSerie } from "@nivo/bump";
 
 interface DataPoint {
     x: string;
     y: number;
 }
 
-// Use BumpDatum to enforce compatibility with ResponsiveBump
-interface Serie extends BumpDatum {
+// Align Serie type with BumpSerie
+interface Serie extends BumpSerie<DataPoint, Record<string, unknown>> {
     id: string;
     data: DataPoint[];
+    [key: string]: any; // Allow additional properties
 }
 
 const MyResponsiveBump = ({ data }: { data: Serie[] }) => (
     <ResponsiveBump
-        data={data as BumpDatum[]} // Type assertion ensures compatibility
+        data={data}
         colors={{ scheme: "spectral" }}
         margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
         lineWidth={3}
@@ -43,7 +44,6 @@ const MyResponsiveBump = ({ data }: { data: Serie[] }) => (
             legend: "",
             legendPosition: "middle",
             legendOffset: -36,
-            truncateTickAt: 0,
         }}
         axisBottom={{
             tickSize: 5,
@@ -52,7 +52,6 @@ const MyResponsiveBump = ({ data }: { data: Serie[] }) => (
             legend: "",
             legendPosition: "middle",
             legendOffset: 32,
-            truncateTickAt: 0,
         }}
         axisLeft={{
             tickSize: 5,
@@ -61,314 +60,33 @@ const MyResponsiveBump = ({ data }: { data: Serie[] }) => (
             legend: "ranking",
             legendPosition: "middle",
             legendOffset: -40,
-            truncateTickAt: 0,
         }}
         axisRight={null}
     />
 );
 
 export default function General() {
-    const data = [
-      {
-        "id": "Serie 1",
-        "data": [
-          {
-            "x": "2000",
-            "y": 12
-          },
-          {
-            "x": "2001",
-            "y": 4
-          },
-          {
-            "x": "2002",
-            "y": 9
-          },
-          {
-            "x": "2003",
-            "y": 4
-          },
-          {
-            "x": "2004",
-            "y": 2
-          }
-        ]
-      },
-      {
-        "id": "Serie 2",
-        "data": [
-          {
-            "x": "2000",
-            "y": 9
-          },
-          {
-            "x": "2001",
-            "y": 9
-          },
-          {
-            "x": "2002",
-            "y": 8
-          },
-          {
-            "x": "2003",
-            "y": 8
-          },
-          {
-            "x": "2004",
-            "y": 6
-          }
-        ]
-      },
-      {
-        "id": "Serie 3",
-        "data": [
-          {
-            "x": "2000",
-            "y": 11
-          },
-          {
-            "x": "2001",
-            "y": 1
-          },
-          {
-            "x": "2002",
-            "y": 5
-          },
-          {
-            "x": "2003",
-            "y": 7
-          },
-          {
-            "x": "2004",
-            "y": 1
-          }
-        ]
-      },
-      {
-        "id": "Serie 4",
-        "data": [
-          {
-            "x": "2000",
-            "y": 4
-          },
-          {
-            "x": "2001",
-            "y": 11
-          },
-          {
-            "x": "2002",
-            "y": 1
-          },
-          {
-            "x": "2003",
-            "y": 5
-          },
-          {
-            "x": "2004",
-            "y": 9
-          }
-        ]
-      },
-      {
-        "id": "Serie 5",
-        "data": [
-          {
-            "x": "2000",
-            "y": 8
-          },
-          {
-            "x": "2001",
-            "y": 10
-          },
-          {
-            "x": "2002",
-            "y": 3
-          },
-          {
-            "x": "2003",
-            "y": 9
-          },
-          {
-            "x": "2004",
-            "y": 11
-          }
-        ]
-      },
-      {
-        "id": "Serie 6",
-        "data": [
-          {
-            "x": "2000",
-            "y": 10
-          },
-          {
-            "x": "2001",
-            "y": 3
-          },
-          {
-            "x": "2002",
-            "y": 12
-          },
-          {
-            "x": "2003",
-            "y": 10
-          },
-          {
-            "x": "2004",
-            "y": 8
-          }
-        ]
-      },
-      {
-        "id": "Serie 7",
-        "data": [
-          {
-            "x": "2000",
-            "y": 1
-          },
-          {
-            "x": "2001",
-            "y": 5
-          },
-          {
-            "x": "2002",
-            "y": 11
-          },
-          {
-            "x": "2003",
-            "y": 12
-          },
-          {
-            "x": "2004",
-            "y": 7
-          }
-        ]
-      },
-      {
-        "id": "Serie 8",
-        "data": [
-          {
-            "x": "2000",
-            "y": 2
-          },
-          {
-            "x": "2001",
-            "y": 7
-          },
-          {
-            "x": "2002",
-            "y": 7
-          },
-          {
-            "x": "2003",
-            "y": 11
-          },
-          {
-            "x": "2004",
-            "y": 4
-          }
-        ]
-      },
-      {
-        "id": "Serie 9",
-        "data": [
-          {
-            "x": "2000",
-            "y": 6
-          },
-          {
-            "x": "2001",
-            "y": 8
-          },
-          {
-            "x": "2002",
-            "y": 4
-          },
-          {
-            "x": "2003",
-            "y": 3
-          },
-          {
-            "x": "2004",
-            "y": 12
-          }
-        ]
-      },
-      {
-        "id": "Serie 10",
-        "data": [
-          {
-            "x": "2000",
-            "y": 5
-          },
-          {
-            "x": "2001",
-            "y": 2
-          },
-          {
-            "x": "2002",
-            "y": 6
-          },
-          {
-            "x": "2003",
-            "y": 1
-          },
-          {
-            "x": "2004",
-            "y": 3
-          }
-        ]
-      },
-      {
-        "id": "Serie 11",
-        "data": [
-          {
-            "x": "2000",
-            "y": 7
-          },
-          {
-            "x": "2001",
-            "y": 6
-          },
-          {
-            "x": "2002",
-            "y": 10
-          },
-          {
-            "x": "2003",
-            "y": 6
-          },
-          {
-            "x": "2004",
-            "y": 10
-          }
-        ]
-      },
-      {
-        "id": "Serie 12",
-        "data": [
-          {
-            "x": "2000",
-            "y": 3
-          },
-          {
-            "x": "2001",
-            "y": 12
-          },
-          {
-            "x": "2002",
-            "y": 2
-          },
-          {
-            "x": "2003",
-            "y": 2
-          },
-          {
-            "x": "2004",
-            "y": 5
-          }
-        ]
-      }
+    const data: Serie[] = [
+        {
+            id: "Serie 1",
+            data: [
+                { x: "2000", y: 12 },
+                { x: "2001", y: 4 },
+                { x: "2002", y: 9 },
+                { x: "2003", y: 4 },
+                { x: "2004", y: 2 },
+            ],
+        },
+        {
+            id: "Serie 2",
+            data: [
+                { x: "2000", y: 9 },
+                { x: "2001", y: 9 },
+                { x: "2002", y: 8 },
+                { x: "2003", y: 8 },
+                { x: "2004", y: 6 },
+            ],
+        },
     ];
 
     return (
